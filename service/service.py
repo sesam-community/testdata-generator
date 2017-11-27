86ee96c2f63daeb06e2d034931889d7ce921863e
Axel Borge <axel.borge@gmail.com>
2017-11-27 13:58:10 +0100

    Small fixes

--- service/service.py
+++ service/service.py
@@ -69,11 +69,11 @@ def stop_and_disable_pipes(pipes):
         if "disable" in pump.supported_operations:
             pump.disable()
 
-def get_staged_entities(api_connection, pipe, stage="sink"):
+def get_staged_entities(api_connection, pipe, stage="sink", limit="10000"):
 
-    result = api_connection.session.get(api_connection.sesamapi_base_url + "/pipes/%s/entities?stage=%s" % (pipe.id, stage))
+    result = api_connection.session.get(api_connection.sesamapi_base_url + "/pipes/%s/entities?stage=%s&limit=%s" % (pipe.id, stage, limit))
     if result.status_code != 200:
-        result.raise_for_status()
+        return iter([])
     return iter(result.json())
 
 def get_testentities(master_node, dataset_dep, pipe_dep, datasets, pipes, datasource, source_entity_id):
@@ -87,14 +87,26 @@ def get_testentities(master_node, dataset_dep, pipe_dep, datasets, pipes, dataso
         if pipe.config['effective']['source']["type"] not in ["dataset","merge"]:
             logger.info("Found source entity '%s' in dataset '%s'..." % (entity["_id"], datasource))
             id = entity["_id"].split(":")[-1]
-            new_entity = [pipe.id, entity]
-            for pipe_entity in get_staged_entities(master_node["api_connection"],pipe, stage="before-transforms"):
+            new_entity = [[pipe.id, entity]]
+            found = False
+            for pipe_entity in get_staged_entities(master_node["api_connection"],pipe, stage="source"):
                 if "_id" in pipe_entity:
                     if pipe_entity["_id"] == id:
+                        logger.info("Found pipe source entity '%s' in pipe '%s'..." % (pipe_entity["_id"], pipe.id))
                         new_entity = [[pipe.id, pipe_entity]]
+                        found = True
                         break
                 else:
-                    logger.warning("Find entity '%s' in dataset '%s'..." % (source_entity_id, datasource))
+                    logger.warning("No _id on pipe source entity '%s' in pipe '%s'. Adding sample to entity" % (source_entity_id, pipe.id))
+                    new_entity.extend([[pipe.id, pipe_entity]])
+                    found = True
+
+            if not found:
+                logger.warning("No matching pipe source entity '%s' in pipe '%s'. Adding sample to entity" % (source_entity_id, pipe.id))
+                if 'pipe_entity' in locals():
+                    new_entity.extend([[pipe.id, pipe_entity]])
+            pipe_entities = new_entity
+
         else:
             for dataset_id in pipe_dep[pipe.id]:
                 dataset = next((x for x in datasets if x.id == dataset_id), None)
@@ -113,7 +125,7 @@ def get_testentities(master_node, dataset_dep, pipe_dep, datasets, pipes, dataso
 
     return pipe_entities
 
-def generate_testdata(master_node, target_node):
+def generate_testdata(master_node, target_node, full_sync):
 
     logger.info("Generate testdata...")
 
@@ -146,6 +158,7 @@ def generate_testdata(master_node, target_node):
 
 
     master_pipes = get_target_pipes(master_node)
+
     source_pipes = get_source_pipes(master_node)
 
 
@@ -172,16 +185,26 @@ def generate_testdata(master_node, target_node):
             test_data.extend(pipe_entities)
 
 
-    for pipe in pipes:
+    for pipe_id in source_pipes:
+        pipe = next((x for x in pipes if x.id == pipe_id), None)
         indexes = { i for i, x in enumerate(test_data) if x[0] == pipe.id }
+        target_pipe = target_node["api_connection"].get_pipe(pipe.id)
+        config = pipe.config.get("original")
+        source = config["source"]
+        entities = []
         if indexes:
-            target_pipe = target_node["api_connection"].get_pipe(pipe.id)
-            config = pipe.config.get("original")
-            source = config["source"]
-            entities = []
+            logger.info("Adding %s propper test entities to pipe '%s'" % (len(indexes), pipe.id))
             for index in indexes:
                 entities.extend([test_data[index][1]])
             json_ent = entity_json.entities_to_json(entities)
+        elif full_sync:
+            logger.info("Adding 5 unrelated test entities to pipe '%s'" % (pipe.id))
+            try:
+                json_ent =  json.dumps(list(get_staged_entities(master_node["api_connection"], pipe, stage="source", limit="5")))
+            except BaseException as e:
+                logger.exception("Error: %s in pipe %s, skipping testdata" % (e, pipe.id))
+                continue
+        if entities != []:
             new_source = {
                             "type": "conditional",
                             "alternatives": {
@@ -249,6 +272,10 @@ if __name__ == '__main__':
     logger.setLevel(logging.DEBUG)
 
     # Read config from env vars
+    if "FULL_SYNC" not in os.environ:
+        full_sync = False
+    else:
+        full_sync = json.loads(os.environ["FULL_SYNC"])
 
     if "MASTER_NODE" not in os.environ:
         logger.error("MASTER_NODE configuration missing!")
@@ -291,15 +318,17 @@ if __name__ == '__main__':
                                                            jwt_auth_token=target_node["jwt_token"],
                                                            timeout=60*10)
 
-    logger.info("Duplicate master and target...")
-    config = master_node["api_connection"].get_config()
-    #target_node["api_connection"].upload_config(config)
-    #copy_environment_variables(master_node, target_node)
-    #stop_and_disable_pipes(target_node["api_connection"].get_pipes())
-    vars = master_node["api_connection"].get_env_vars()
-    vars.update({"node-env": "test"})
-    target_node["api_connection"].put_env_vars(vars)
+    if full_sync:
+        logger.info("Duplicate master and target...")
+        config = master_node["api_connection"].get_config()
+        target_node["api_connection"].upload_config(config)
+        copy_environment_variables(master_node, target_node)
+        stop_and_disable_pipes(target_node["api_connection"].get_pipes())
+        #vars = master_node["api_connection"].get_env_vars()
+        #vars.update({"node-env": "test"})
+        target_node["api_connection"].post_env_vars({"node-env": "test"})
 
-    generate_testdata(master_node, target_node)
+
+    generate_testdata(master_node, target_node, full_sync)
 
 
